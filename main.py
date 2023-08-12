f562fd431cca45c00d3f37eec6ba853ff0f2a77f
Wendy <wkcordova@espe.edu.ec>
2023-08-12 00:20:48 -0500

    Adding ingresos

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field
@@ -6,6 +7,9 @@ from config.database import Session, engine, Base
 from models.modelos import Usuarios as UsuariosModel, Gastos as GastosModel,Ingresos as IngresosModel
 #from models.modelos import Ingresos as IngresosModel
 from fastapi.encoders import jsonable_encoder
+from fastapi import Path
+from fastapi import HTTPException
+from datetime import date
 
 app = FastAPI()
 app.title = 'APP EXPENSE TRACKER API'
@@ -52,6 +56,29 @@ class Gasto(BaseModel):
                 "categoria": "Sueldo",
                 "valor": 454.32,
                 "fechagasto": "2023-01-01"
+            }
+        }
+
+#----------------------------------Clase Ingresos
+class Ingreso(BaseModel):
+        
+    idingresos: int 
+    idusuario: int
+    concepto: str = Field(min_length=0, max_length=100)
+    monto:float
+    descripcion: str = Field(min_length=0, max_length=100)
+    periodicidad:str = Field(min_length=0, max_length=100)
+    fechaingreso: date 
+    class Config:
+        schema_extra = {
+            "example": {
+                "idingresos": 1,
+                "idusuario": 1751112839,
+                "concepto": "Gano rifa",
+                "monto": 455.36,
+                "descripcion": "dinero rifa",
+                "periodicidad": "anual",
+                "fechaingreso": "2023-01-01"
             }
         }
 
@@ -147,3 +174,67 @@ def delete_gastos(idgastos: int)-> dict:
     db.delete(result)
     db.commit()
     return JSONResponse(status_code=200, content={"message": "Se ha eliminado el gasto"})
+
+#----------------------------------------------------------------------------
+
+@app.get('/gastos/{categoria}', tags=['gastos'], response_model=List[Gasto], status_code=200)
+def get_gastos_por_categoria(categoria: str) -> List[Gasto]:
+    db = Session()
+    result = db.query(GastosModel).filter(GastosModel.categoria == categoria).all()
+    
+    if not result:
+        raise HTTPException(status_code=404, detail='No se encontraron gastos en esa categoría')
+
+    return JSONResponse(status_code=200, content=jsonable_encoder(result))
+#-----------------------------------------------------------------------------------------------
+
+#Metodos de ingresos-----------------------------------------------------------
+
+#Método Post de ingresos
+@app.post('/ingresos', tags=['ingresos'], response_model=dict, status_code=201)
+def create_ingresos(ingreso: Ingreso) -> dict:
+    db = Session()
+    new_ingresos = IngresosModel(**ingreso.dict())
+    db.add(new_ingresos)
+    db.commit()
+    return JSONResponse(status_code=201, content={"message": "Se ha registrado el ingreso"})
+
+#------------------------------
+#Método Get de ingresos
+@app.get('/ingresos', tags=['ingresos'], response_model=List[Ingreso], status_code=200)
+def get_ingresos() -> List[Ingreso]:
+    db = Session()
+    result = db.query(IngresosModel).all()
+    return JSONResponse(status_code=200, content = jsonable_encoder(result))
+
+#Método PUT de ingresos
+@app.put('/ingresos/{idingresos}', tags=['ingresos'], response_model=dict, status_code=200)
+def update_ingresos(idingresos: int, ingreso: Ingreso)-> dict:
+    db = Session()
+    result = db.query(IngresosModel).filter(IngresosModel.idingresos == idingresos).first()
+    if not result:
+        return JSONResponse(status_code=404, content={'message': "No encontrado"})
+   # result.idgastos=Gasto.idgastos
+    #result.idusuariogastos=Gasto.idusuariogastos
+    result.concepto= ingreso.concepto
+    result.monto=ingreso.monto
+    result.descripcion= ingreso.descripcion
+    result.periodicidad=ingreso.periodicidad
+    result.fechaingreso = ingreso.fechaingreso
+    
+
+    db.commit()
+    return JSONResponse(status_code=200, content={"message": "Se ha modificado los gastos"})
+
+#-------------------------------
+#método DELETE de ingresos
+
+@app.delete('/ingresos/{idingresos}', tags=['ingresos'], response_model=dict, status_code=200)
+def delete_ingresos(idingresos: int)-> dict:
+    db = Session()
+    result = db.query(IngresosModel).filter(IngresosModel.idingresos == idingresos).first()
+    if not result:
+        return JSONResponse(status_code=404, content={'message': "No encontrado"})
+    db.delete(result)
+    db.commit()
+    return JSONResponse(status_code=200, content={"message": "Se ha eliminado el ingreso"})
