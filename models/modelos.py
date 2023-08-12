2df8cd03a3cc7931f992153cb13abaac3d958629
mxnrenato <maximilianoriveraabad@gmail.com>
2023-08-11 20:50:37 -0500

    Adding Ingresos

--- models/modelos.py
+++ models/modelos.py
@@ -1,5 +1,6 @@
 from config.database import Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, Date, ForeignKey
 
 class Usuarios(Base):
     __tablename__ = "usuarios"
@@ -9,7 +10,21 @@ class Usuarios(Base):
     cedula = Column(Integer, primary_key = True)
     email = Column(String)
     password = Column(String)
-    fechanacimiento = Column(String)
+    fechanacimiento = Column(Date)
     direccion = Column(String)
     telefono = Column(Integer)
 
+    ingresos = relationship("Ingresos", back_populates="usuario")
+
+class Ingresos(Base):
+    __tablename__ = "Ingresos"
+
+    idingresos = Column(Integer, primary_key = True)
+    idusuario = Column(Integer, ForeignKey("usuarios.cedula"))
+    concepto = Column(String)
+    descripcion = Column(String)
+    periodicidad = Column(String)
+    fechaingreso = Column(Date)
+
+    usuario = relationship("Usuarios", back_populates="ingresos")
+    
